9cc00014cef586e58e927225be012b29f442520d
Alan <sinderpl@gmail.com>
2023-02-06 20:29:16 +0000

    Add return object

--- app/pkg/controller/sampleroute/sampleroute.go
+++ app/pkg/controller/sampleroute/sampleroute.go
@@ -2,6 +2,7 @@ package sampleroutes
 
 import (
 	"fmt"
+	"math/rand"
 	"net/http"
 
 	"github.com/labstack/echo"
@@ -14,12 +15,18 @@ func Routes(router *echo.Group) {
 	router.DELETE("/sample/:id", deleteSample)
 }
 
+type Sample struct {
+	ID   int64  `json:"ID"`
+	Name string `json:"name"`
+}
+
 type RequestBody struct {
-	UserName string `json:"userName"`
+	UserName string `json:"userName,omitempty"`
 }
 
 type StatusResponse struct {
 	Message string `json:"message"`
+	Sample  Sample `json:"sample"`
 }
 
 func getSample(c echo.Context) error {
@@ -38,9 +45,22 @@ func getSample(c echo.Context) error {
 }
 
 func createSample(c echo.Context) error {
+	requestBody := &RequestBody{}
+
+	err := c.Bind(requestBody)
+	if err != nil {
+		return err
+	}
+
+	id := rand.Int63()
+	newObject := Sample{
+		ID:   id,
+		Name: fmt.Sprintf("New Object %d", id),
+	}
 
 	body := &StatusResponse{
-		Message: "Sample Create !",
+		Message: fmt.Sprintf("Sample Create ! User name: %s", requestBody.UserName),
+		Sample:  newObject,
 	}
 
 	return c.JSON(http.StatusOK, body)
