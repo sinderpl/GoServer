0a30c09d8f8098f0ec589206d180d853d69a7375
Alan <sinderpl@gmail.com>
2023-04-07 19:24:10 +0100

    tests

--- app/pkg/controller/sampleroute/sampleroute_test.go
+++ app/pkg/controller/sampleroute/sampleroute_test.go
@@ -1,1 +1,44 @@
 package sampleroutes
+
+import (
+	"github.com/labstack/echo"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestGetSample(t *testing.T) {
+	t.Run("Test simple get", func(t *testing.T) {
+		//request, _ := http.NewRequest(http.MethodGet, "/sample", nil)
+		//response := httptest.NewRecorder()
+
+		//routes.InitializeRouter()
+
+		e := echo.New()
+		req := httptest.NewRequest(http.MethodGet, "/", nil)
+		rec := httptest.NewRecorder()
+		c := e.NewContext(req, rec)
+		c.SetPath("/sample/")
+		c.SetParamNames("userName")
+		c.SetParamValues("John")
+
+		// Assertions
+		//if assert.NoError(t, h.getUser(c)) {
+		assert.Equal(t, http.StatusOK, rec.Code)
+		//assert.Equal(t, userJSON, rec.Body.String())
+		//}
+	})
+}
+
+func TestPostSample(t *testing.T) {
+
+}
+
+func TestPatchSample(t *testing.T) {
+
+}
+
+func TestDeleteSample(t *testing.T) {
+
+}
